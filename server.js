c7b646ecd60185e726e87a9b66fb571e4025fc23
Mark <markherrington5@gmail.com>
2019-03-04 23:19:09 -0500

    final push before due

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
