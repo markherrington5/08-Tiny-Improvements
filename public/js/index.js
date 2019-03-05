2a3ed888dc4deaff67dca88fa439a43e5eb65e97
Mark <markherrington5@gmail.com>
2019-03-04 22:58:51 -0500

    finished through index.js

--- public/js/index.js
+++ public/js/index.js
@@ -15,3 +15,54 @@ const render = function (dataList) {
         );
     };
 };
+
+const getKudos = function () {
+    jQuery.get('/api/kudo').then(function(data) {
+        render(data)
+    });
+}
+
+const getUsers = function () {
+    $.get('api/user').then(function (data) {
+        for (let i = 0; i < data.length; i++) {
+            $('#kudo-from').append(
+                `<option value='${data[i].id}'>${data[i].name}</option>`
+                )
+            $('#kudo-to').append(
+                `<option value='${data[i].id}'>${data[i].name}</option>`
+                )
+        }
+    })
+};
+
+const postKudo = function (event) {
+    event.preventDefault();
+    $('#messages').empty();
+
+    if($('#kudo-from').val() && $('#kudo-to').val()){
+        const kudo = {
+            title: $('#kudo-title').val().trim(),
+            body: $('#kudo-body').val().trim(),
+            from: $('#kudo-from').val().trim(),
+            to: $('#kudo-to').val().trim(),
+        }
+
+        $.post('api/kudo', kudo).then(function(data) {
+            $('#kudo-title').val('');
+            $('#kudo-body').val('');
+            $('#kudo-from').val('');
+            $('#kudo-to').val('');
+            $('.modal').modal('hide');
+            getKudos();
+        }).fail(function(err){
+            $('#messages').append(`<div class='alert alert-danger'>There was a problem with your submission.</div>`)
+        })
+    } else {
+        $('#messages').append(`<div class='alert alert-danger'>Please select both a sender and recipient.</div>`)
+    }
+}
+
+getKudos();
+getUsers();
+
+$(document).on('click', '#send-kudo', postKudo)
