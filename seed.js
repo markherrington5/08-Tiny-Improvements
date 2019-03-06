f829614861ca220c7304de31700a17b226bd02b5
Mark <markherrington5@gmail.com>
2019-03-06 14:59:54 -0500

    fixed seed.js

--- seed.js
+++ seed.js
@@ -1,6 +1,54 @@
 // use improvements;
+const mongoose = require("mongoose");
 
-db.users.insert({name:'Glen'});
-db.users.insert({name:'Brenda'});
-db.users.insert({name:'Craig'});
-db.users.insert({name:'Hanna'});
+const User = require("./models/user");
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/improvements';
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+User.bulkWrite([
+    {
+        insertOne: {
+            document: {
+                name: "Craig"
+            }
+        }
+    },
+    {
+        insertOne: {
+            document: {
+                name: "Craigbert"
+            }
+        }
+    },
+    {
+        insertOne: {
+            document: {
+                name: "Craigalina"
+            }
+        }
+    },
+    {
+        insertOne: {
+            document: {
+                name: "Craiggy"
+            }
+        }
+    },
+    {
+        insertOne: {
+            document: {
+                name: "Craigson"
+            }
+        }
+    },
+    {
+        insertOne: {
+            document: {
+                name: "Craigster"
+            }
+        }
+    }
+]).then(function () {
+    mongoose.connection.close();
+});
